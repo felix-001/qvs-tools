0b534cfd39f74e3ac93eba1293a6cb3487e48ab6
rigensen <mr.right.0005@gmail.com>
2021-05-31 23:24:28 +0800

    解析invite日志ok

--- tools.go
+++ tools.go
@@ -1,65 +1,100 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"log"
 	"os/exec"
+	"strings"
 )
 
 type ConsoleParams struct {
 	logPath string
+	gbid    string
+	chid    string
 }
 
 func Exec(cmdstr string) (string, error) {
 	cmd := exec.Command("bash", "-c", cmdstr)
 	b, err := cmd.CombinedOutput()
 	if err != nil {
-		log.Println(err)
+		log.Println(cmdstr, err)
 		return "", err
 	}
 	return string(b), err
 }
 
 func GetLatestLogFile(path string) (string, error) {
-	cmdstr := "ls -t " + path + "qvs-log.log* | head -n 1"
-	return Exec(cmdstr)
+	cmdstr := "ls -t " + path + "qvs-sip.log* | head -n 1"
+	logFile, err := Exec(cmdstr)
+	if err != nil {
+		return "", err
+	}
+	return logFile[:len(logFile)-1], err
 }
 
 type LogParser struct {
 	logFile string
-	gbId    string
-	chId    string
+	gbid    string
+	chid    string
 }
 
-func NewLogParser(logFile string) *LogParser {
-	return &LogParser{logFile: logFile}
+func NewLogParser(logFile, gbid, chid string) *LogParser {
+	return &LogParser{logFile: logFile, gbid: gbid, chid: chid}
 }
 
 type InviteInfo struct {
-	ssrc  int
+	ssrc  string
 	rtpIp string
 	time  string
 }
 
 func (self *LogParser) getLastInviteLog() (string, error) {
-	cmdstr := "tac " + self.logFile + " | grep \"sip_invite&chid=" + self.chId + "&id=" + self.gbId + "-m 1"
+	cmdstr := "tac " + self.logFile + " | grep \"sip_invite&chid=" + self.chid + "&id=" + self.gbid + "\" -m 1"
 	return Exec(cmdstr)
 }
 
-func (self *LogParser) GetInviteInfo() *InviteInfo {
+func (self *LogParser) GetInviteInfo() (info *InviteInfo, err error) {
+	inviteLog, err := self.getLastInviteLog()
+	if err != nil {
+		return nil, err
+	}
+	start := strings.Index(inviteLog, "[2021")
+	if start == -1 {
+		log.Println("get start error")
+		return nil, errors.New("get start error")
+	}
+	end := strings.Index(inviteLog, "]")
+	if end == -1 {
+		log.Println("get end error")
+		return nil, errors.New("get end error")
+	}
+	info = &InviteInfo{}
+	info.time = inviteLog[start+1 : end]
 
+	start = strings.Index(inviteLog, "ip=")
+	end = strings.Index(inviteLog, "&rtp_por")
+	info.rtpIp = inviteLog[start+3 : end]
+
+	start = strings.Index(inviteLog, "ssrc=")
+	end = strings.Index(inviteLog, "&token")
+	info.ssrc = inviteLog[start+5 : end]
+
+	return
 }
 
 func main() {
 	log.SetFlags(log.Lshortfile)
 	logPath := flag.String("logpath", "~/qvs-sip/_package/run/", "log file path")
+	gbid := flag.String("gbid", "", "gbid")
+	chid := flag.String("chid", "", "chid")
 	flag.Parse()
-	params := &ConsoleParams{}
-	params.logPath = *logPath
-	log.Println(params.logPath)
-	logFile, _ := GetLatestLogFile(params.logPath)
+	logFile, _ := GetLatestLogFile(*logPath)
 	log.Println(logFile)
-	parser := NewLogParser(logFile)
-	inviteInfo := parser.GetInviteInfo()
+	parser := NewLogParser(logFile, *gbid, *chid)
+	inviteInfo, err := parser.GetInviteInfo()
+	if err != nil {
+		return
+	}
 	log.Printf("%+v\n", inviteInfo)
 }
