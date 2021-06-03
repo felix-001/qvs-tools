cebfe51bbc4b27395ea3d18779281c908603eaf8
liyq <liyuanquan@qiniu.com>
2021-06-03 19:05:02 +0800

    fetch qvs-sip and qvs-rtp log done

--- tools.go
+++ tools.go
@@ -3,7 +3,6 @@ package main
 import (
 	"errors"
 	"flag"
-	"fmt"
 	"log"
 	"os/exec"
 	"strings"
@@ -13,19 +12,25 @@ func Exec(cmdstr string) (string, error) {
 	cmd := exec.Command("bash", "-c", cmdstr)
 	b, err := cmd.CombinedOutput()
 	if err != nil {
-		log.Println(cmdstr, err)
+		log.Println("cmd:", cmdstr, "err:", err)
 		return "", err
 	}
 	return string(b), err
 }
 
-func (self *LogManager) GetLatestLogFile() (string, error) {
+func (self *LogManager) GetLatestSipLogFile() (string, error) {
 	cmdstr := "ls -t " + self.logPath + "/qvs-sip.log* | head -n 1"
 	logFile, err := Exec(cmdstr)
 	if err != nil {
 		return "", err
 	}
 	return logFile[:len(logFile)-1], err
+}
+
+func (self *LogManager) GetRtpLogFromNode(rtpNode string) error {
+	cmdstr := "qscp qboxserver@" + rtpNode + ":/home/qboxserver/qvs-rtp/_package/run/qvs-rtp.log* ~/logs/"
+	_, err := Exec(cmdstr)
+	return err
 }
 
 type LogParser struct {
@@ -51,7 +56,8 @@ type InviteInfo struct {
 
 func (self *LogParser) getLastInviteLog() (string, error) {
 	cmdstr := "tac " + self.logFile + " | grep \"sip_invite&chid=" + self.chid + "&id=" + self.gbid + "\" -m 1"
-	return Exec(cmdstr)
+	s, err := Exec(cmdstr)
+	return s, err
 }
 
 func (self *LogManager) GetLogsFromJJh1445() (string, error) {
@@ -60,19 +66,22 @@ func (self *LogManager) GetLogsFromJJh1445() (string, error) {
 }
 
 func (self *LogParser) GetInviteInfo() (info *InviteInfo, err error) {
-	inviteLog, err := self.getLastInviteLog()
-	if err != nil {
-		return nil, err
+	inviteLog, _err := self.getLastInviteLog()
+	if _err != nil {
+		err = _err
+		return
 	}
 	start := strings.Index(inviteLog, "[2021")
 	if start == -1 {
 		log.Println("get start error")
-		return nil, errors.New("get start error")
+		err = errors.New("get start error")
+		return
 	}
 	end := strings.Index(inviteLog, "]")
 	if end == -1 {
 		log.Println("get end error")
-		return nil, errors.New("get end error")
+		err = errors.New("get end error")
+		return
 	}
 	info = &InviteInfo{}
 	info.time = inviteLog[start+1 : end]
@@ -96,14 +105,21 @@ func NewLogManager(logPath string) *LogManager {
 	return &LogManager{logPath: logPath}
 }
 
-func (self *LogParser) GetNodeIdFromPdr() {
-
+func (self *LogParser) GetNodeIdFromPdr(rtpIp string) (string, error) {
+	cmdstr := "~/bin/getRtpNodeId.py " + rtpIp
+	res, err := Exec(cmdstr)
+	if err != nil {
+		return "", err
+	}
+	return res[:len(res)-1], nil
 }
 
-// todo
-// pdr搜索
-// [2021-05-31 23:20:18.672][Trace][24719][733g0873] HTTP #0 127.0.0.1:12660 GET http://42.123.110.67:1985/api/v1/gb28181?action=create_channel&app=3nm4x0ulb1fl0&g7112aac_enable=go&id=31011500991180001563_34020000001310000111&port_mode=fixed&protocol=udp&push=42.123.110.67&real=222.208.242.45&token=CZsxhjEqdmcNMrSQfUqM, content-length=-1
-// 这个日志，去获取rtp node id
+// 拉流失败
+// 1.有没有503
+// 2.有没有inviting
+// 3.tcp/udp有没有收到包
+// 4.connect reset by peer?
+// 5.ssrc illegal?
 
 func main() {
 	log.SetFlags(log.Lshortfile)
@@ -116,18 +132,34 @@ func main() {
 		return
 	}
 	logMgr := NewLogManager(*logPath)
-	res, err := logMgr.GetLogsFromJJh1445()
-	fmt.Println(res)
+	log.Println("start to fetch log file from jjh1445 ~/qvs-sip/_package/run")
+	_, err := logMgr.GetLogsFromJJh1445()
+	//fmt.Println(res)
 	if err != nil {
 		log.Println(err)
 		return
 	}
-	logFile, _ := logMgr.GetLatestLogFile()
-	log.Println(logFile)
-	parser := NewLogParser(logFile, *gbid, *chid, *logPath)
-	inviteInfo, err := parser.GetInviteInfo()
+	logFile, err := logMgr.GetLatestSipLogFile()
 	if err != nil {
 		return
 	}
+	log.Println("fetch log file: " + logFile + " done")
+	parser := NewLogParser(logFile, *gbid, *chid, *logPath)
+	inviteInfo, err := parser.GetInviteInfo()
+	if err != nil {
+		log.Println("get invite log err")
+		return
+	}
 	log.Printf("%+v\n", inviteInfo)
+	nodeId, err := parser.GetNodeIdFromPdr(inviteInfo.rtpIp)
+	if err != nil {
+		return
+	}
+	log.Println("rtp NodeId:", nodeId)
+	log.Println("start to get qvs-rtp log from", nodeId)
+	err = logMgr.GetRtpLogFromNode(nodeId)
+	if err != nil {
+		return
+	}
+	log.Println("fetch qvs-rtp log done")
 }
