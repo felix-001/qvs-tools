c09b0c8855e645da0a0df045268690ea46d94873
rigensen <mr.right.0005@gmail.com>
2021-06-26 12:55:43 +0800

    多次播放语音ok

--- talk-test.go
+++ talk-test.go
@@ -10,8 +10,9 @@ import (
 	"time"
 )
 
-const StreamUrl = "http://localhost:1985/api/v1/gb28181?action=talk&id=31010403001370002272&is_new=true&base64_pcm="
-const InviteUrl = "http://localhost:7279/api/v1/gb28181?action=sip_invite&chid=31010403001370002272&id=31010403001370002272&ip=100.100.81.214&rtp_port=9001&rtp_proto=udp&is_talk=true&ssrc="
+const RtpIp = "192.168.1.5"
+const StreamUrl = "http://localhost:1985/api/v1/gb28181?action=talk&id=31010403001370002272&base64_pcm="
+const InviteUrl = "http://localhost:7279/api/v1/gb28181?action=sip_invite&chid=31010403001370002272&id=31010403001370002272&ip=" + RtpIp + "&rtp_port=9001&rtp_proto=udp&is_talk=true&ssrc="
 const ByeUrl = "http://localhost:7279/api/v1/gb28181?action=sip_bye&chid=31010403001370002272&id=31010403001370002272&&is_talk=true&call_id="
 const BlkLen = 5 * 1024
 
@@ -55,9 +56,12 @@ func InviteAudio(ssrc int) string {
 	return inviteResp.Callid
 }
 
-func SendBlk(blk []byte) int {
+func SendBlk(blk []byte, is_first bool) int {
 	base64Blk := base64.RawURLEncoding.EncodeToString(blk)
 	streamUrl := StreamUrl + base64Blk
+	if is_first {
+		streamUrl += "&is_new=true"
+	}
 	resp, err := http.Get(streamUrl)
 	if err != nil {
 		log.Println(err)
@@ -113,16 +117,18 @@ func main() {
 		return
 	}
 	pos := 0
-	alreadyInvite := false
 	callid := ""
+	is_first := true
 	for pos < len(pcm) {
 		blkLen := CalcBlkLen(len(pcm), pos)
 		blk := pcm[pos : pos+blkLen]
-		ssrc := SendBlk(blk)
-		if !alreadyInvite {
+		if is_first {
+			ssrc := SendBlk(blk, true)
 			log.Println("ssrc", ssrc)
 			callid = InviteAudio(ssrc)
-			alreadyInvite = true
+			is_first = false
+		} else {
+			SendBlk(blk, false)
 		}
 		pos += BlkLen
 	}
