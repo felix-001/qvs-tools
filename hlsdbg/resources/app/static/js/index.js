541cc0609021d1514a27c62ba0435a47296e2a0b
liyq <liyuanquan@qiniu.com>
2022-02-15 15:15:48 +0800

    多行列布局ok

--- hlsdbg/resources/app/static/js/index.js
+++ hlsdbg/resources/app/static/js/index.js
@@ -5,11 +5,123 @@ let index = {
         asticode.modaler.setContent(c);
         asticode.modaler.show();
     },
+    createLineChart: function() {
+        let ctx = document.createElement("canvas");
+        document.getElementById("files_panel").append(ctx);
+        var c = new Chart(ctx, {
+            // 要创建的图表类型
+            type: 'line',
+        
+            // 数据集
+            data: {
+                labels: ["January", "February", "March", "April", "May", "June", "July"],
+                datasets: [{
+                    label: "My First dataset",
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: [0, 10, 5, 2, 20, 30, 45],
+                }]
+            },
+        
+            // 配置选项
+            options: {}
+        });
+        return c
+    },
     init: function() {
         // Init
         asticode.loader.init();
         asticode.modaler.init();
         asticode.notifier.init();
+
+        let ctx = document.createElement("canvas");
+        document.getElementById("files_panel").append(ctx);
+        var c = new Chart(ctx, {
+            // 要创建的图表类型
+            type: 'line',
+        
+            // 数据集
+            data: {
+                labels: ["January", "February", "March", "April", "May", "June", "July"],
+                datasets: [{
+                    label: "My First dataset",
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: [0, 10, 5, 2, 20, 30, 45],
+                }]
+            },
+        
+            // 配置选项
+            options: {}
+        });
+        c0 = this.createLineChart("c0")
+        ids = ["c0", "c1", "c2", "c3"]
+        for (id in ids) {
+            c = 
+        }
+
+        let ctx1 = document.createElement("canvas");
+        document.getElementById("c2").append(ctx1);
+        var c = new Chart(ctx1, {
+            // 要创建的图表类型
+            type: 'line',
+        
+            // 数据集
+            data: {
+                labels: ["January", "February", "March", "April", "May", "June", "July"],
+                datasets: [{
+                    label: "My First dataset",
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: [0, 10, 5, 2, 20, 30, 45],
+                }]
+            },
+        
+            // 配置选项
+            options: {}
+        });
+
+        let ctx2 = document.createElement("canvas");
+        document.getElementById("c3").append(ctx2);
+        var c = new Chart(ctx2, {
+            // 要创建的图表类型
+            type: 'line',
+        
+            // 数据集
+            data: {
+                labels: ["January", "February", "March", "April", "May", "June", "July"],
+                datasets: [{
+                    label: "My First dataset",
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: [0, 10, 5, 2, 20, 30, 45],
+                }]
+            },
+        
+            // 配置选项
+            options: {}
+        });
+
+        let ctx4 = document.createElement("canvas");
+        document.getElementById("c4").append(ctx4);
+        var c = new Chart(ctx4, {
+            // 要创建的图表类型
+            type: 'line',
+        
+            // 数据集
+            data: {
+                labels: ["January", "February", "March", "April", "May", "June", "July"],
+                datasets: [{
+                    label: "My First dataset",
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: [0, 10, 5, 2, 20, 30, 45],
+                }]
+            },
+        
+            // 配置选项
+            options: {}
+        });
 
         // Wait for astilectron to be ready
         document.addEventListener('astilectron-ready', function() {
