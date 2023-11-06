541720c9a1a2e0e7aab213765ee35e1400803403
rigensen <mr.right.0005@gmail.com>
2023-11-06 22:30:54 +0800

    重构

--- qvsdbg/parser.go
+++ qvsdbg/parser.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"regexp"
 	"strings"
@@ -38,85 +40,136 @@ func (s *Parser) searchLogs(node, service, re string) (string, error) {
 	return RunCmd(cmd)
 }
 
-type KeyWord struct {
+func (s *Parser) getValue(line, start, end string) (string, bool) {
+	reg := fmt.Sprintf("%s(.*?)%s", start, end)
+	re := regexp.MustCompile(reg)
+	matchs := re.FindStringSubmatch(line)
+	if len(matchs) < 1 {
+		return "", false
+	}
+	return strings.TrimSpace(matchs[1]), true
+}
+
+type Keyword struct {
 	Start string
 	End   string
 	Key   string
 }
 
-type Result struct {
-	Key   string
-	Value string
+type Rule struct {
+	Match    string
+	Keywords []Keyword
 }
 
-func re(start, end string) string {
-	return fmt.Sprintf("%s(.*)%s", start, end)
-}
-
-func (s *Parser) parseLine(line string, keywords []KeyWord) ([]Result, error) {
-	regex := re(keywords[0].Start, keywords[1].End)
-	for i := 1; i < len(keywords); i++ {
-		regex += ".*?" + re(keywords[i].Start, keywords[i].End)
-	}
-	re := regexp.MustCompile(regex)
-	matchs := re.FindStringSubmatch(line)
-	if len(matchs) < len(keywords) {
-		return nil, fmt.Errorf("parse %s err", line)
-	}
-	results := []Result{}
-	for i, match := range matchs {
-		results = append(results, Result{Key: keywords[i].Key, Value: match})
-	}
-	return results, nil
+var rules = []Rule{
+	{
+		Match: "invite ok",
+		Keywords: []Keyword{
+			{
+				Start: "rtpAccessIp:",
+				End:   "callId",
+				Key:   "rtpIp",
+			},
+			{
+				Start: "callId:",
+				End:   "____",
+				Key:   "callId",
+			},
+			{
+				Start: "ssrc:",
+				End:   "host",
+				Key:   "ssrc",
+			},
+			{
+				Start: "rtpPort:",
+				End:   "$",
+				Key:   "rtpPort",
+			},
+		},
+	},
 }
 
 func (s *Parser) parseInviteBye(data string) error {
-	log.Println(data)
-	//scanner := bufio.NewScanner(strings.NewReader(data))
-	lines := strings.Split(data, "\n")
-	for _, line := range lines {
-		//line := scanner.Text()
-		log.Println(line)
-		keywords := []KeyWord{
-			{
-				Key:   "rtpIp",
-				Start: "rtpAccessIp",
-				End:   "callId",
-			},
-			{
-				Key:   "callId",
-				Start: "callId",
-				End:   "___",
-			},
-			{
-				Key:   "ssrc",
-				Start: "ssrc:",
-				End:   "host",
-			},
-			{
-				Key:   "rtpPort",
-				Start: "rtpPort:",
-				End:   "$",
-			},
+	scanner := bufio.NewScanner(strings.NewReader(data))
+	for scanner.Scan() {
+		line := scanner.Text()
+		result := M{}
+		for _, rule := range rules {
+			re := regexp.MustCompile(rule.Match)
+			if re.MatchString(line) {
+				for _, keyword := range rule.Keywords {
+					val, match := s.getValue(line, keyword.Start, keyword.End)
+					if match {
+						result[keyword.Key] = val
+					}
+				}
+			}
 		}
-		results, err := s.parseLine(line, keywords)
-		if err != nil {
-			//log.Println(err)
-			return err
+		for k, v := range result {
+			log.Println(k, v)
 		}
-		log.Println(results)
+		/*
+			rtpIp, match := s.getValue(line, "rtpAccessIp:", "callId")
+			if match {
+				log.Println("rtpIp:", rtpIp)
+			}
+			callid, match := s.getValue(line, "callId:", "____")
+			if match {
+				log.Println("callid:", callid)
+			}
+			ssrc, match := s.getValue(line, "ssrc:", "host")
+			if match {
+				log.Println("ssrc:", ssrc)
+			}
+			rtpPort, match := s.getValue(line, "rtpPort:", "$")
+			if match {
+				log.Println("rtpPort:", rtpPort)
+			}
+			/*
+			/*
+				keywords := []KeyWord{
+					{
+						Key:   "rtpIp",
+						Start: "rtpAccessIp",
+						End:   "callId",
+					},
+					{
+						Key:   "callId",
+						Start: ": ",
+						End:   "___",
+					},
+					{
+						Key:   "ssrc",
+						Start: "ssrc:",
+						End:   "host",
+					},
+					{
+						Key:   "rtpPort",
+						Start: "rtpPort:",
+						End:   "$",
+					},
+				}
+				results, err := s.parseLine(line, keywords)
+				if err != nil {
+					log.Println(err)
+					return err
+				}
+				if len(results) > 0 {
+					log.Println(line)
+					log.Println(results)
+				}
+		*/
 	}
-	/*
-		if err := scanner.Err(); err != nil {
-			fmt.Println("Error:", err)
-		}
-	*/
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error:", err)
+	}
 	return nil
 }
 
 func (s *Parser) inviteBye() error {
 	data := ""
-	nodes := []string{"jjh1445", "jjh1449", "jjh250", "bili-jjh9"}
+	//nodes := []string{"jjh1445", "jjh1449", "jjh250", "bili-jjh9"}
+	nodes := []string{"jjh1445"}
 	for _, node := range nodes {
 		invite := fmt.Sprintf("invite ok.*%s", s.Conf.GbId)
 		bye := fmt.Sprintf("bye ok.*%s", s.Conf.GbId)
@@ -131,6 +184,11 @@ func (s *Parser) inviteBye() error {
 	}
 	if err := s.parseInviteBye(data); err != nil {
 		log.Println(err)
+	}
+	err := ioutil.WriteFile("test.txt", []byte(data), 0644)
+	if err != nil {
+		log.Println(err)
+		return err
 	}
 	return nil
 }
