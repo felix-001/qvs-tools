d94de4db661d66fc1bcd674c0b5f9de8ad5df7c6
rigensen <mr.right.0005@gmail.com>
2024-04-05 22:22:27 +0800

    update

--- douyu-ratio/main.go
+++ douyu-ratio/main.go
@@ -1,6 +1,14 @@
 package main
 
-import "log"
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+)
 
 func streamAnalysis(streamid string) {
 	usedNodes := []string{}
@@ -26,7 +34,19 @@ func streamAnalysis(streamid string) {
 	log.Println(usedNodes, len(usedNodes))
 }
 
+func douyuSourceUrl() {
+	expireTime := time.Now().Unix() + 600
+	hexTime := strconv.FormatInt(expireTime, 16)
+	raw := fmt.Sprintf("%s%s%s", os.Args[2], os.Args[1], hexTime)
+	hash := md5.Sum([]byte(raw))
+	txSecret := hex.EncodeToString([]byte(hash[:]))
+	originUrl := fmt.Sprintf("http://%s/%s/%s.flv?txSecret=%s&txTime=%s", "qngin.douyucdn.cn",
+		"douyu", os.Args[1], txSecret, hexTime)
+	log.Println(originUrl)
+}
+
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
-	streamAnalysis("85894rmovieChow_4000h")
+	//streamAnalysis("85894rmovieChow_4000h")
+	douyuSourceUrl()
 }
