73287fbce8b19c4965bda141326a5452c91aee26
liyq <liyuanquan@qiniu.com>
2024-07-16 10:06:18 +0800

    update

--- middle-source-analysis/streams.go
+++ middle-source-analysis/streams.go
@@ -7,9 +7,10 @@ import (
 	"time"
 )
 
-func (s *Parser) getStreamSourceNodeMap(bkt string) map[string][]string {
+func (s *Parser) getStreamSourceNodeMap(bkt string) (map[string][]string, map[string][]string) {
 	streamSourceNodesMap := make(map[string][]string)
-	for node, streamInfo := range s.nodeStremasMap {
+	notServingNodesMap := make(map[string][]string)
+	for nodeId, streamInfo := range s.nodeStremasMap {
 		for _, stream := range streamInfo.Streams {
 			if stream.Bucket != bkt {
 				continue
@@ -17,16 +18,25 @@ func (s *Parser) getStreamSourceNodeMap(bkt string) map[string][]string {
 			if stream.RelayBandwidth == 0 || stream.RelayType != 2 {
 				continue
 			}
-			streamSourceNodesMap[stream.Key] = append(streamSourceNodesMap[stream.Key], node)
+			node, ok := s.allNodesMap[nodeId]
+			if !ok {
+				log.Println("getStreamSourceNodeMap", node.Id, "not found in allNodesMap")
+				continue
+			}
+			if node.RuntimeStatus != "Serving" {
+				notServingNodesMap[nodeId] = append(notServingNodesMap[nodeId], nodeId)
+				continue
+			}
+			streamSourceNodesMap[stream.Key] = append(streamSourceNodesMap[stream.Key], nodeId)
 		}
 	}
-	return streamSourceNodesMap
+	return streamSourceNodesMap, notServingNodesMap
 }
 
-var streamRatioHdr = "流ID, 在线人数, 边缘节点个数, ROOT节点个数, 回源节点个数, 回源带宽, 放大比, 回源节点详情\n"
+var streamRatioHdr = "流ID, 在线人数, 边缘节点个数, ROOT节点个数, 回源节点个数, 非serving的回源节点个数, 回源带宽, 放大比, 回源节点详情, 非serving回源节点详情\n"
 
 func (s *Parser) dumpStreamsDetail(bkt string) {
-	streamSourceNodesMap := s.getStreamSourceNodeMap(bkt)
+	streamSourceNodesMap, notServingNodesMap := s.getStreamSourceNodeMap(bkt)
 	streamRatioCsv := streamRatioHdr
 	for streamId, streamDetail := range s.streamDetailMap {
 		var streamTotalBw, streamTotalRelayBw float64
@@ -40,10 +50,11 @@ func (s *Parser) dumpStreamsDetail(bkt string) {
 				streamTotalRootNodeCount += len(streamInfo.RootNodes)
 			}
 		}
-		streamRatioCsv += fmt.Sprintf("%s, %d, %d, %d, %d, %.1f, %.1f, %+v\n", streamId,
-			streamTotalOnlineNum, streamTotalEdgeNodeCount,
-			streamTotalRootNodeCount, len(streamSourceNodesMap[streamId]),
-			streamTotalRelayBw, streamTotalBw/streamTotalRelayBw, streamSourceNodesMap[streamId])
+		streamRatioCsv += fmt.Sprintf("%s, %d, %d, %d, %d, %d, %.1f, %.1f, %+v, %+v\n", streamId,
+			streamTotalOnlineNum, streamTotalEdgeNodeCount, streamTotalRootNodeCount,
+			len(streamSourceNodesMap[streamId]), len(notServingNodesMap[streamId]),
+			streamTotalRelayBw, streamTotalBw/streamTotalRelayBw, streamSourceNodesMap[streamId],
+			notServingNodesMap[streamId])
 	}
 	file := fmt.Sprintf("streams-%d.csv", time.Now().Unix())
 	err := ioutil.WriteFile(file, []byte(streamRatioCsv), 0644)
