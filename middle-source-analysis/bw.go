c06cae2f11ca70b81e703cecd5250a069ab7665b
liyq <liyuanquan@qiniu.com>
2024-08-21 19:35:13 +0800

    rawTotalMaxBwGbpsAferIpv6数值跟L战役的portal能对得上

--- middle-source-analysis/bw.go
+++ middle-source-analysis/bw.go
@@ -14,18 +14,62 @@ import (
 	zlog "github.com/rs/zerolog/log"
 )
 
+const (
+	TotalMaxBwGbps                  = "totalMaxBwGbps"
+	TotalOutBwGbps                  = "totalOutBwGbps"
+	TotalAvailableBwGbps            = "totalAvailableBwGbps"
+	RawTotalMaxBwGbps               = "rawTotalMaxBwGbps"
+	RawTotalMaxBwGbpsAferIpv6       = "rawTotalMaxBwGbpsAferIpv6"
+	RawTotalMaxBwGbpsAferProbeState = "rawTotalMaxBwGbpsAferProbeState"
+	RawTotalMaxBwGbpsAferProbeSpeed = "rawTotalMaxBwGbpsAferProbeSpeed"
+	SpecialNodesMaxBwGbps           = "specialNodesMaxBwGbps"
+	MikuTotalOutBwGbps              = "mikuTotalOutBwGbps"
+
+	MikuLowUtilizationNodeCnt         = "mikuLowUtilizationNodeCnt"
+	NodeUnavailableCnt                = "nodeUnavailableCnt"
+	NodeNoPortsCnt                    = "nodeNoPortsCnt"
+	TotalDynNodeCnt                   = "totalDynNodeCnt"
+	AvailableDynNodeCnt               = "availableDynNodeCnt"
+	TimeLimitNodeCnt                  = "timeLimitNodeCnt"
+	BanTransProvNodeCnt               = "banTransProvNodeCnt"
+	AvailableDynNodeAfterTimeLimitCnt = "availableDynNodeAfterTimeLimitCnt"
+	IpV6Cnt                           = "ipV6Cnt"
+	PrivateIpCnt                      = "privateIpCnt"
+	NetProbeStateErrIpCnt             = "netProbeStateErrIpCnt"
+	NetProbeSpeedErrIpCnt             = "netProbeSpeedErrIpCnt"
+	AvailableIpCnt                    = "availableIpCnt"
+)
+
+func (s *Parser) getNodeReportBw(node *model.RtNode, bwMap map[string]float64) float64 {
+	var reportBw float64
+	nodeStreams := s.nodeStremasMap[node.Id]
+	for _, streamInfo := range nodeStreams.Streams {
+		for _, player := range streamInfo.Players {
+			for _, ipInfo := range player.Ips {
+				bwMap[MikuTotalOutBwGbps] += float64(ipInfo.Bandwidth*8) / 1000000000.0
+				reportBw += float64(ipInfo.Bandwidth * 8 / 1000000)
+				//fmt.Println(player.Protocol, float64(ipInfo.Bandwidth*8)/1000000, ipInfo.Ip, streamInfo.Key)
+			}
+		}
+	}
+	return reportBw
+}
+
+func (s *Parser) dumpBw(bwMap map[string]float64, counterMap map[string]int) {
+	for k, bw := range bwMap {
+		fmt.Printf("%s: %.0f\n", k, bw)
+	}
+	for k, cnt := range counterMap {
+		fmt.Printf("%s: %d\n", k, cnt)
+	}
+}
+
 func (s *Parser) CalcTotalBw() {
-
-	var totalMaxBwGbps float64 = 0
-	var rawTotalMaxBwGbps float64 = 0
-	var totalOutBwGbps float64 = 0
-	var specialMaxBw float64 = 0
-
-	var mikuLowRatioNodeCnt = 0
+	bwMap := make(map[string]float64)
+	counterMap := make(map[string]int)
+	customerMap := make(map[uint32]int)
 
 	machinelist := ""
-
-	customerMap := make(map[uint32]int)
 
 	logger := zlog.Logger
 	logger = logger.Level(3)
@@ -34,128 +78,84 @@ func (s *Parser) CalcTotalBw() {
 		if !node.IsDynamic {
 			continue
 		}
-		s.TotalDynNoeCnt++
+		counterMap[TotalDynNodeCnt]++
 		if !util.CheckNodeUsable(logger, node, consts.TypeLive) {
 			//log.Printf("checkNode nodeId:%s, machineId:%s check not pass, type: %s\n", node.Id, node.MachineId, node.ResourceType)
-			s.NodeUnavailableCnt++
+			counterMap[NodeUnavailableCnt]++
 			continue
 		}
 		if !checkDynamicNodesPort(node) {
-			s.NodeNoPortsCnt++
+			counterMap[NodeNoPortsCnt]++
 			continue
 		}
-		s.AvailableDynNodeCnt++
+		counterMap[AvailableDynNodeCnt]++
 		if !checkCanScheduleOfTimeLimit(node, 3600) {
-			s.TimeLimitCnt++
+			counterMap[TimeLimitNodeCnt]++
 			continue
 		}
 		if node.IsBanTransProv {
-			s.BanTransProvNodeCnt++
-			//continue
+			counterMap[BanTransProvNodeCnt]++
 		}
 		for _, customerId := range node.CustomerIds {
 			customerMap[customerId]++
 		}
-		if !ContainInIntSlice(1380460970, node.CustomerIds) {
-			continue
-		}
-		if node.MachineId != "a71163883d96c375deef28ef8612b242" {
-			continue
-		}
-		s.AvailableDynNodeAfterTimeLimitCnt++
+		counterMap[AvailableDynNodeAfterTimeLimitCnt]++
+
 		machinelist += node.MachineId + "\n"
-		var totalMikuBw float64 = 0
+
 		var nodeMaxOutBw float64 = 0
 		var nodeOutBw float64 = 0
+
 		for _, ipInfo := range node.Ips {
 			if publicUtil.IsPrivateIP(ipInfo.Ip) {
-				s.PrivateIpCnt++
+				counterMap[PrivateIpCnt]++
 				continue
 			}
-			rawTotalMaxBwGbps += ipInfo.MaxOutMBps * 8 / 1000
+			bwMap[RawTotalMaxBwGbps] += ipInfo.MaxOutMBps * 8 / 1000
 			if ipInfo.IsIPv6 {
-				s.IpV6Cnt++
+				counterMap[IpV6Cnt]++
 				continue
 			}
+			bwMap[RawTotalMaxBwGbpsAferIpv6] += ipInfo.MaxOutMBps * 8 / 1000
 			if ipInfo.IPStreamProbe.State != model.StreamProbeStateSuccess {
-				s.NetProbeStateErrIpCnt++
+				counterMap[NetProbeStateErrIpCnt]++
 				continue
 			}
+			bwMap[RawTotalMaxBwGbpsAferProbeState] += ipInfo.MaxOutMBps * 8 / 1000
 			if ipInfo.IPStreamProbe.Speed < 8 && ipInfo.IPStreamProbe.MinSpeed < 6 {
-				s.NetProbeSpeedErrIpCnt++
+				counterMap[NetProbeSpeedErrIpCnt]++
 				continue
 			}
+			bwMap[RawTotalMaxBwGbpsAferProbeSpeed] += ipInfo.MaxOutMBps * 8 / 1000
 			if ContainInStringSlice(node.MachineId, SepcialNodeList) {
-				specialMaxBw += ipInfo.MaxInMBps * 8 / 1000
+				bwMap[SpecialNodesMaxBwGbps] += ipInfo.MaxInMBps * 8 / 1000
 			}
-			s.AvailableIpCnt++
-			totalMaxBwGbps += ipInfo.MaxOutMBps * 8
-			totalOutBwGbps += ipInfo.OutMBps * 8
+			counterMap[AvailableIpCnt]++
+			bwMap[TotalMaxBwGbps] += ipInfo.MaxOutMBps * 8 / 1000
+			bwMap[TotalOutBwGbps] += ipInfo.OutMBps * 8 / 1000
 			nodeMaxOutBw += ipInfo.MaxOutMBps * 8
 			nodeOutBw += ipInfo.OutMBps * 8
-			//totalMaxBwGbps += ipInfo.MaxOutMBps * 8 / 1000
-			//totalOutBwGbps += ipInfo.OutMBps * 8 / 1000
-			//totalBwGbps += ipInfo.MaxOutMBps
 		}
 
-		nodeStreams := s.nodeStremasMap[node.Id]
-		for _, streamInfo := range nodeStreams.Streams {
-			for _, player := range streamInfo.Players {
-				for _, ipInfo := range player.Ips {
-					totalMikuBw += float64(ipInfo.Bandwidth * 8 / 1000000)
-					fmt.Println(player.Protocol, float64(ipInfo.Bandwidth*8)/1000000, ipInfo.Ip, streamInfo.Key)
-				}
-			}
-		}
-		ratio := totalMikuBw / nodeOutBw
-		if ratio < 0.3 && nodeMaxOutBw > 9000 {
-			log.Println(node.Id, node.MachineId, totalMikuBw, nodeMaxOutBw, nodeOutBw)
-			mikuLowRatioNodeCnt++
+		reportBw := s.getNodeReportBw(node, bwMap)
+		ratio := reportBw / nodeOutBw
+		if ratio < 0.3 {
+			//log.Println(node.Id, node.MachineId, totalMikuBw, nodeMaxOutBw, nodeOutBw)
+			counterMap[MikuLowUtilizationNodeCnt]++
 		}
 	}
+	bwMap[TotalAvailableBwGbps] = bwMap[TotalMaxBwGbps] - bwMap[TotalOutBwGbps]
 
-	fmt.Printf(`totalMaxBwGbps: %.0fGbps
-mikuLowRatioNodeCnt: %d
-rawTotalMaxBwGbps: %.0fMbps
-totalOutBwGbps: %.0fMbps
-specialMaxBw: %.0fGbps
-NodeUnavailableCnt: %d
-NodeNoPortsCnt: %d
-PrivateIpCnt: %d
-NetProbeStateErrIpCnt: %d
-NetProbeStateErrIpCnt: %d
-IpV6Cnt: %d
-TotalDynNoeCnt: %d
-AvailableDynNodeCnt: %d
-AvailableDynNodeAfterTimeLimitCnt: %d
-AvailableIpCnt: %d
-BanTransProvNodeCnt: %d
-timelimitCnt: %d
-`,
-		totalMaxBwGbps,
-		mikuLowRatioNodeCnt,
-		rawTotalMaxBwGbps,
-		totalOutBwGbps,
-		specialMaxBw,
-		s.NodeUnavailableCnt,
-		s.NodeNoPortsCnt,
-		s.PrivateIpCnt,
-		s.NetProbeStateErrIpCnt,
-		s.NetProbeSpeedErrIpCnt,
-		s.IpV6Cnt,
-		s.TotalDynNoeCnt,
-		s.AvailableDynNodeCnt,
-		s.AvailableDynNodeAfterTimeLimitCnt,
-		s.AvailableIpCnt,
-		s.BanTransProvNodeCnt,
-		s.TimeLimitCnt)
+	s.dumpBw(bwMap, counterMap)
 
-	log.Println("customer info:")
-	for cursomerId, cnt := range customerMap {
-		fmt.Println(cursomerId, CustomerIdMap[cursomerId], cnt)
-	}
+	/*
+		log.Println("customer info:")
+		for cursomerId, cnt := range customerMap {
+			fmt.Println(cursomerId, CustomerIdMap[cursomerId], cnt)
+		}
+	*/
 
-	file := fmt.Sprintf("machinelist-%d.csv", time.Now().Unix())
+	file := fmt.Sprintf("machinelist-%d.txt", time.Now().Unix())
 	err := ioutil.WriteFile(file, []byte(machinelist), 0644)
 	if err != nil {
 		log.Println(err)
