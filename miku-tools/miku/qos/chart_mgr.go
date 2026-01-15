172bf1bd5d674f642bac8e7c1f98e0aeaa94e9d2
liyq <liyuanquan@qiniu.com>
2026-01-15 14:46:40 +0800

    update

--- miku-tools/miku/qos/chart_mgr.go
+++ miku-tools/miku/qos/chart_mgr.go
@@ -199,27 +199,30 @@ WHERE 1=1
 	return sql, nil
 }
 
-func (c *ChartMgr) getDimensionValue(req QOSRequest, result map[string]any, chartConf *config.ChartConf) (string, string) {
+func (c *ChartMgr) getDimensionValue(req QOSRequest, field string, result map[string]any, chartConf *config.ChartConf) string {
 	dimensionValue := chartConf.SeriesTitle
-	resultField := ""
 	if chartConf.SQL.Dimension != "" {
 		if _, ok := result[chartConf.SQL.Dimension]; !ok {
 			log.Printf("err, dimension nil, result: %+v\n", result)
-			return "", ""
+			return ""
 		}
 		if result[chartConf.SQL.Dimension] == nil {
 			log.Printf("err, dimension nil, result: %+v\n", result)
-			return "", ""
+			return ""
 		}
 		dimensionValue = result[chartConf.SQL.Dimension].(string)
 	}
 	//log.Println("fields:", chartConf.SQL.Fields)
-	for _, field := range chartConf.SQL.Fields {
-		if result[field] != nil {
-			dimensionValue += "_" + field
-			resultField = field
+	/*
+		for _, field := range chartConf.SQL.Fields {
+			if result[field] != nil {
+				log.Println("result:", result[field])
+				dimensionValue += "_" + field
+				resultField = field
+			}
 		}
-	}
+	*/
+	dimensionValue += "_" + field
 	if req.Protocol != "" {
 		dimensionValue += "_" + req.Protocol
 	}
@@ -236,7 +239,7 @@ func (c *ChartMgr) getDimensionValue(req QOSRequest, result map[string]any, char
 		dimensionValue += "_" + req.StreamID
 	}
 
-	return dimensionValue, resultField
+	return dimensionValue
 }
 
 func (c *ChartMgr) getLineData(req QOSRequest, results []map[string]any, chartConf *config.ChartConf) any {
@@ -248,19 +251,21 @@ func (c *ChartMgr) getLineData(req QOSRequest, results []map[string]any, chartCo
 	}
 	lastTs := ""
 	for _, result := range results {
-		dimensionValue, field := c.getDimensionValue(req, result, chartConf)
-		if dimensionValue == "" || field == "" {
-			log.Println("err, dimensionValue or field is empty", "dimensionValue:", dimensionValue, "field:", field)
-			continue
-		}
-		if _, ok := seriesData[dimensionValue]; !ok {
-			seriesData[dimensionValue] = &SeriesData{}
-		}
-		seriesData[dimensionValue].YAxis = append(seriesData[dimensionValue].YAxis, result[field].(string))
-		t := result["ts_m"].(time.Time).Format("2006-01-02 15:04:05")
-		if lastTs == "" || t != lastTs {
-			data.XAxis = append(data.XAxis, t)
-			lastTs = t
+		for _, field := range chartConf.SQL.Fields {
+			dimensionValue := c.getDimensionValue(req, field, result, chartConf)
+			if dimensionValue == "" {
+				log.Println("err, dimensionValue or field is empty", "dimensionValue:", dimensionValue, "field:", field)
+				continue
+			}
+			if _, ok := seriesData[dimensionValue]; !ok {
+				seriesData[dimensionValue] = &SeriesData{}
+			}
+			seriesData[dimensionValue].YAxis = append(seriesData[dimensionValue].YAxis, result[field].(string))
+			t := result["ts_m"].(time.Time).Format("2006-01-02 15:04:05")
+			if lastTs == "" || t != lastTs {
+				data.XAxis = append(data.XAxis, t)
+				lastTs = t
+			}
 		}
 
 	}
