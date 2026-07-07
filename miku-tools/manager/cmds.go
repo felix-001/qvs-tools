6066193cb69e39322f75ae20b78a9f4706d1d472
liyq <liyuanquan@qiniu.com>
2026-07-07 15:17:07 +0800

    update

--- miku-tools/manager/cmds.go
+++ miku-tools/manager/cmds.go
@@ -124,10 +124,22 @@ func (m *CommandManager) CmdInvite() *Command {
 
 func (m *CommandManager) CmdKodo() *Command {
 	handler := func() {
+		util.DownloadIpdb(m.config)
+	}
+	cmd := &Command{
+		Desc: "使用 ipdb 配置(ipdb.ips_source_param)中的 remote_url 从 kodo 下载 ipdb 文件到 /tmp 目录\n" +
+			"  依赖配置项 ak/sk/remote_url/retry_count, 下载后保存为 /tmp/<ipv4|ipv6>.ipdb",
+		Handler: handler,
+	}
+	return cmd
+}
+
+func (m *CommandManager) CmdSign() *Command {
+	handler := func() {
 		util.SignResource(m.config)
 	}
 	cmd := &Command{
-		Desc:    "kodo",
+		Desc:    "kodo 私有资源签名, -uid <uid> -key <key> -domain <domain>",
 		Handler: handler,
 	}
 	return cmd
--- miku-tools/public/util/kodo.go
+++ miku-tools/public/util/kodo.go
@@ -3,8 +3,13 @@ package util
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 	"mikutool/config"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
 	"strconv"
 	"time"
 
@@ -12,6 +17,8 @@ import (
 	"github.com/qbox/bo-sdk/sdk/qconf/appg"
 	"github.com/qbox/bo-sdk/sdk/qconf/qconfapi"
 	"github.com/qbox/pili/base/qiniu/api/auth/digest"
+	"github.com/qbox/pili/base/qiniupkg.com/api.v7/kodo"
+	ipdb "github.com/qbox/pili/common/ipdb.v1"
 	urlescape "github.com/qiniu/x/url"
 )
 
@@ -49,6 +56,95 @@ func SignResource(conf *config.Config) {
 		return
 	}
 	fmt.Println(addr)
+}
+
+// DownloadIpdb 使用 ipdb 配置(ipdb.ips_source_param)中的 remote_url 从 kodo 下载 ipdb 文件到 /tmp 目录。
+// 参考 pili ipdb.v1 中 cityWorker.loadRemote 的实现，通过 kodo.Client 生成私有下载链接后拉取文件。
+func DownloadIpdb(conf *config.Config) {
+	if len(conf.IPDB.IP) == 0 {
+		log.Println("no ipdb config, check ipdb.ips_source_param in mikutool.yaml")
+		return
+	}
+	for name, param := range conf.IPDB.IP {
+		if param == nil || param.RemoteUrl == "" {
+			log.Printf("ipdb [%s] has no remote_url, skip\n", name)
+			continue
+		}
+		if err := downloadIpdbFromKodo(name, param); err != nil {
+			log.Printf("download ipdb [%s] err: %v\n", name, err)
+			continue
+		}
+	}
+}
+
+func downloadIpdbFromKodo(name string, param *ipdb.IPSourceReqParam) error {
+	u, err := url.Parse(param.RemoteUrl)
+	if err != nil {
+		return fmt.Errorf("parse remote_url %s err: %w", param.RemoteUrl, err)
+	}
+	if u.Path == "" || u.Path == "/" {
+		return fmt.Errorf("remote_url %s has no key path", param.RemoteUrl)
+	}
+
+	key := u.Path[1:]
+	baseUrl := kodo.MakeBaseUrl(u.Host, key)
+	kc := kodo.New(0, &kodo.Config{
+		AccessKey: param.AK,
+		SecretKey: param.SK,
+		RSHost:    baseUrl,
+	})
+
+	deadline := time.Now().Add(time.Second * 3600).Unix()
+	policy := &kodo.GetPolicy{
+		Expires: uint32(deadline),
+	}
+	privateUrl := kc.MakePrivateUrl(kc.RSHost, policy)
+	privateAccessUrl := fmt.Sprintf("%s&now=%d", privateUrl, time.Now().Unix())
+	log.Printf("ipdb [%s] load privateAccessUrl: %s\n", name, privateAccessUrl)
+
+	dst := filepath.Join("/tmp", fmt.Sprintf("%s.ipdb", name))
+
+	client := &http.Client{Timeout: 60 * time.Second}
+	retry := int(param.RemoteRetry)
+	if retry <= 0 {
+		retry = 1
+	}
+
+	var lastErr error
+	for i := 0; i < retry; i++ {
+		if lastErr = httpDownloadFile(client, privateAccessUrl, dst); lastErr != nil {
+			log.Printf("download ipdb [%s] attempt %d/%d err: %v\n", name, i+1, retry, lastErr)
+			continue
+		}
+		log.Printf("download ipdb [%s] success, saved to %s\n", name, dst)
+		return nil
+	}
+	return lastErr
+}
+
+func httpDownloadFile(client *http.Client, downloadUrl, dst string) error {
+	resp, err := client.Get(downloadUrl)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode/100 != 2 {
+		return fmt.Errorf("http.Get %s resp.StatusCode %d", downloadUrl, resp.StatusCode)
+	}
+
+	f, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	n, err := io.Copy(f, resp.Body)
+	if err != nil {
+		return fmt.Errorf("write %s err: %w", dst, err)
+	}
+	log.Printf("downloaded %d bytes to %s\n", n, dst)
+	return nil
 }
 
 func SignURL(ctx context.Context, uid uint32, scheme, domain, key string, ttl int64) (string, error) {
